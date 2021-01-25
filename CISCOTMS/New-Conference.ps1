16a1f5c37395710b9d518762d512ca19e8965a42
chida <christian@damberg.org>
2021-01-25 11:24:56 +0100

    Update and rename New-ConferenceRequest.ps1 to New-Conference.ps1

--- CISCOTMS/New-Conference.ps1
+++ CISCOTMS/New-Conference.ps1
@@ -5,7 +5,7 @@
     Created on:   	1/12/2021 10:42 AM
     Created by:   	Christian Damberg
     Organization: 	Cygate AB
-    Filename:     	New-ConferenceRequest.ps1
+    Filename:     	New-Conference.ps1
     ===========================================================================
     .DESCRIPTION
     This script creats an xml for sending a request to CISCO TNS
