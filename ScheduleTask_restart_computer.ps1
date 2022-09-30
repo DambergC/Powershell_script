9a49b7970e672b13293d73ab388ff0e12de68cb2
chida <christian@damberg.org>
2022-09-30 12:12:56 +0200

    Update ScheduleTask_restart_computer.ps1

--- ScheduleTask_restart_computer.ps1
+++ ScheduleTask_restart_computer.ps1
@@ -1,20 +1,27 @@
+
+# Here you set the date and time for reboot
 $ParamsTrigger = @{
     Once = $true
-    At   = Get-Date "2022-09-30 09:50:00"
+    At   = Get-Date "2022-09-30 12:06:00"
 }
 
-#remove old task
+#remove old task if it exist
 Unregister-ScheduledTask -TaskName "Once Reboot" -Confirm:$false -ErrorAction SilentlyContinue
 
 # Create task action
 $taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument 'Restart-Computer -Force'
-# Create a trigger (Mondays at 4 AM)
+
+# Create a trigger
 $taskTrigger = New-ScheduledTaskTrigger @ParamsTrigger
+
 # The user to run the task
-$taskUser = New-ScheduledTaskPrincipal -UserId "LOCALSERVICE" -LogonType ServiceAccount -RunLevel Highest
+$taskUser = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
 # The name of the scheduled task.
 $taskName = "Once Reboot"
+
 # Describe the scheduled task.
-$description = "Forcibly reboot the computerat specific time"
+$description = "Forcibly reboot the computerat specific time once"
+
 # Register the scheduled task
 Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Principal $taskUser -Description $description
